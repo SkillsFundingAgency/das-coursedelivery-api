928f84a67b0152dddc8d0b85f94b4358488b6282
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-23 22:30:13 +0100

    Add provider standard import repository and tests
    Add Get all, delete all and insert many

---
+++ src/SFA.DAS.CourseDelivery.Data.UnitTests/Repository/ProviderStandardImportRepository/WhenDeletingAllItems.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CourseDelivery.Domain.Entities;
+using SFA.DAS.Courses.Data.UnitTests.DatabaseMock;
+
+namespace SFA.DAS.CourseDelivery.Data.UnitTests.Repository.ProviderStandardImportRepository
+{
+    public class WhenDeletingAllItems
+    {
+        private Mock<ICourseDeliveryDataContext> _courseDeliveryDataContext;
+        private Data.Repository.ProviderStandardImportRepository _providerStandardImportRepository;
+        private List<ProviderStandardImport> _providerStandardImports;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _providerStandardImports = new List<ProviderStandardImport>
+            {
+                new ProviderStandardImport
+                {
+                    StandardId = 1,
+                    Ukprn = 123
+                },
+                new ProviderStandardImport
+                {
+                    StandardId = 2,
+                    Ukprn = 123
+                }
+            };
+
+            _courseDeliveryDataContext = new Mock<ICourseDeliveryDataContext>();
+            _courseDeliveryDataContext.Setup(x => x.ProviderStandardImports).ReturnsDbSet(_providerStandardImports);
+            _providerStandardImportRepository = new Data.Repository.ProviderStandardImportRepository(_courseDeliveryDataContext.Object);
+        }
+
+        [Test]
+        public void Then_The_ProviderStandardImports_Are_Removed()
+        {
+            //Act
+            _providerStandardImportRepository.DeleteAll();
+            
+            //Assert
+            _courseDeliveryDataContext.Verify(x=>x.ProviderStandardImports.RemoveRange(_courseDeliveryDataContext.Object.ProviderStandardImports), Times.Once);
+            _courseDeliveryDataContext.Verify(x=>x.SaveChanges(), Times.Once);
+        }
+    }
+}
