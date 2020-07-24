cad36fe2fed8fc9bbcdf39f0940e21c56f7ec235
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-24 10:22:52 +0100

    Add SQL import tables for Provider Standards

---
+++ src/SFA.DAS.CourseDelivery.Database/Tables/ProviderStandard_Import.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ProviderStandard_Import]
+(
+	[StandardId] INT NOT NULL,
+	[UkPrn] INT NOT NULL,
+	[StandardInfoUrl] VARCHAR(1000) NOT NULL,
+	[Email] VARCHAR(256) NOT NULL,
+	[Phone] VARCHAR(50) NOT NULL,
+	[ContactUrl] VARCHAR(500) NOT NULL,
+	CONSTRAINT PK_ProviderStandardImport PRIMARY KEY CLUSTERED ([StandardId], [Ukprn])
+)
+GO
+
---
+++ src/SFA.DAS.CourseDelivery.Database/Tables/Provider_Import.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Provider_Import]
+(
+	[Id] BIGINT PRIMARY KEY,
+	[UkPrn] INT NOT NULL,
+	[Name] VARCHAR(1000) NOT NULL,
+	[TradingName] VARCHAR(1000) NOT NULL,
+	[NationalProvider] BIT NOT NULL DEFAULT 0,
+	[MaxEmployerLevyCap] INT NOT NULL,
+	[EmployerSatisfaction] DECIMAL NOT NULL DEFAULT 0,
+	[LearnerSatisfaction] DECIMAL NOT NULL DEFAULT 0,
+	[Email] VARCHAR(256) NOT NULL,
+	[Phone] VARCHAR(50) NOT NULL,
+	[Website] VARCHAR(500) NOT NULL,
+)
+GO
