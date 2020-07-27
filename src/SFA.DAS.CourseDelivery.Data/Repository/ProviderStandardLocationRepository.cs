d54ebed01fc84612b10196f3ab73f7e7e75242bc
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-27 10:48:01 +0100

    Add ProviderStandardLocation repository
    Added methods for insert many and delete

---
+++ src/SFA.DAS.CourseDelivery.Data/Repository/ProviderStandardLocationRepository.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.CourseDelivery.Domain.Entities;
+
+namespace SFA.DAS.CourseDelivery.Data.Repository
+{
+    public class ProviderStandardLocationRepository
+    {
+        private readonly ICourseDeliveryDataContext _dataContext;
+
+        public ProviderStandardLocationRepository (ICourseDeliveryDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task InsertMany(IEnumerable<ProviderStandardLocation> providerStandardLocations)
+        {
+            await _dataContext.ProviderStandardLocations.AddRangeAsync(providerStandardLocations);
+            _dataContext.SaveChanges();
+        }
+
+        public void DeleteAll()
+        {
+            _dataContext.ProviderStandardLocations.RemoveRange(_dataContext.ProviderStandardLocations);
+            _dataContext.SaveChanges();
+        }
+    }
+}
