31f2bc0c7a1bdf183ade38097e9d19d290933cf1
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-27 10:23:16 +0100

    Add provider repository
    Add methods for insert many and delete all

---
+++ src/SFA.DAS.CourseDelivery.Data.UnitTests/Repository/ProviderRepository/WhenDeletingAllItems.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.CourseDelivery.Domain.Entities;
+using SFA.DAS.Courses.Data.UnitTests.DatabaseMock;
+
+namespace SFA.DAS.CourseDelivery.Data.UnitTests.Repository.ProviderRepository
+{
+    public class WhenDeletingAllItems
+    {
+        private Mock<ICourseDeliveryDataContext> _courseDeliveryDataContext;
+        private Data.Repository.ProviderRepository _providerImportRepository;
+        private List<Provider> _providerImports;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _providerImports = new List<Provider>
+            {
+                new Provider
+                {
+                    Id = 1,
+                    Name ="Test"
+                },
+                new Provider
+                {
+                    Id = 1,
+                    Name= "Test 2"
+                }
+            };
+
+            _courseDeliveryDataContext = new Mock<ICourseDeliveryDataContext>();
+            _courseDeliveryDataContext.Setup(x => x.Providers).ReturnsDbSet(_providerImports);
+            _providerImportRepository = new Data.Repository.ProviderRepository(_courseDeliveryDataContext.Object);
+        }
+
+        [Test]
+        public void Then_The_ProviderImports_Are_Removed()
+        {
+            //Act
+            _providerImportRepository.DeleteAll();
+            
+            //Assert
+            _courseDeliveryDataContext.Verify(x=>x.Providers.RemoveRange(_courseDeliveryDataContext.Object.Providers), Times.Once);
+            _courseDeliveryDataContext.Verify(x=>x.SaveChanges(), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.CourseDelivery.Data/Repository/ProviderRepository.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.CourseDelivery.Domain.Entities;
+
+namespace SFA.DAS.CourseDelivery.Data.Repository
+{
+    public class ProviderRepository
+    {
+        private readonly ICourseDeliveryDataContext _dataContext;
+
+        public ProviderRepository(ICourseDeliveryDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task InsertMany(IEnumerable<Provider> providers)
+        {
+            await _dataContext.Providers.AddRangeAsync(providers);
+            _dataContext.SaveChanges();
+        }
+
+        public void DeleteAll()
+        {
+            _dataContext.Providers.RemoveRange(_dataContext.Providers);
+            _dataContext.SaveChanges();
+        }
+    }
+}
