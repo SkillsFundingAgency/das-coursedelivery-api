a1f4391f1698ab3c87f8bb33a5d451d296d4a6af
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-27 13:07:48 +0100

    Add actual data tables

---
+++ src/SFA.DAS.CourseDelivery.Database/Tables/Provider.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Provider]
+(
+	[Id] BIGINT PRIMARY KEY,
+	[UkPrn] INT NOT NULL,
+	[Name] VARCHAR(1000) NOT NULL,
+	[TradingName] VARCHAR(1000) NULL,
+	[NationalProvider] BIT NOT NULL DEFAULT 0,
+	[EmployerSatisfaction] DECIMAL NOT NULL DEFAULT 0,
+	[LearnerSatisfaction] DECIMAL NOT NULL DEFAULT 0,
+	[Email] VARCHAR(256) NULL,
+	[Phone] VARCHAR(50) NULL,
+	[Website] VARCHAR(500) NULL,
+)
+GO
---
+++ src/SFA.DAS.CourseDelivery.Database/Tables/ProviderStandard.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ProviderStandard]
+(
+	[StandardId] INT NOT NULL,
+	[UkPrn] INT NOT NULL,
+	[StandardInfoUrl] VARCHAR(1000) NULL,
+	[Email] VARCHAR(256) NULL,
+	[Phone] VARCHAR(50) NULL,
+	[ContactUrl] VARCHAR(500) NULL,
+	CONSTRAINT PK_ProviderStandard PRIMARY KEY CLUSTERED ([StandardId], [Ukprn])
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IDX_ProviderStandard_StandardId] ON [dbo].[ProviderStandard] (StandardId) 
+INCLUDE (UkPrn, StandardInfoUrl, Email,  Phone, ContactUrl) WITH (ONLINE = ON) 
+GO 
