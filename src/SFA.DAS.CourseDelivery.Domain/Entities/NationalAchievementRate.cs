e14f33829daabbd8967968ced66d2f341273ca24
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-07 17:49:02 +0100

    Add mapping from NationalAchievementRateImport to entity

--- src/SFA.DAS.CourseDelivery.Domain/Entities/NationalAchievementRate.cs
+++ src/SFA.DAS.CourseDelivery.Domain/Entities/NationalAchievementRate.cs
@@ -3,5 +3,18 @@ namespace SFA.DAS.CourseDelivery.Domain.Entities
     public class NationalAchievementRate : NationalAchievementRateBase
     {
         public virtual Provider Provider { get ; set ; }
+
+        public static implicit operator NationalAchievementRate(NationalAchievementRateImport source)
+        {
+            return new NationalAchievementRate
+            {
+                Age = source.Age,
+                Ukprn = source.Ukprn,
+                ApprenticeshipLevel = source.ApprenticeshipLevel,
+                OverallCohort = source.OverallCohort,
+                OverallAchievementRate = source.OverallAchievementRate,
+                SectorSubjectArea = source.SectorSubjectArea
+            };
+        }
     }
 }
