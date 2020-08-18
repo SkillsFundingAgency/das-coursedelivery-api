4c644402765580ba3e75ae6bc4f7e6cdd19022d9
robyoung91 <robertmatthewyoung@gmail.com>
2020-08-18 15:19:23 +0100

    Add CourseDirectory HealthCheck, tests and infrastructure

---
+++ src/SFA.DAS.CourseDelivery.Infrastructure/HealthCheck/CourseDirectoryHealthCheck.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SFA.DAS.CourseDelivery.Domain.Entities;
+using SFA.DAS.CourseDelivery.Domain.Interfaces;
+using SFA.DAS.Courses.Infrastructure.HealthCheck;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.CourseDelivery.Infrastructure.HealthCheck
+{
+    public class CourseDirectoryHealthCheck : IHealthCheck
+    {
+        private const string HealthCheckResultDescription = "Course Directory Health Check";
+        private readonly IImportAuditRepository _importData;
+        public CourseDirectoryHealthCheck(IImportAuditRepository importData)
+        {
+            _importData = importData;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var timer = Stopwatch.StartNew();
+            var latestCourseDirectoryData = await _importData.GetLastImportByType(ImportType.CourseDirectory);
+
+            timer.Stop();
+            var durationString = timer.Elapsed.ToHumanReadableString();
+
+            if (DateTime.UtcNow >= latestCourseDirectoryData.TimeStarted.AddHours(25))
+            {
+                return new HealthCheckResult(HealthStatus.Degraded, "Course directory data load is over 25 hours old", null, new Dictionary<string, object> { { "Duration", durationString } });
+            }
+
+            if (latestCourseDirectoryData.RowsImported == 0)
+            {
+                return new HealthCheckResult(HealthStatus.Degraded, "Course directory data load has imported zero rows", null, new Dictionary<string, object> { { "Duration", durationString } });
+            }
+
+            return HealthCheckResult.Healthy(HealthCheckResultDescription, new Dictionary<string, object> { { "Duration", durationString } });
+        }
+    }
+}
