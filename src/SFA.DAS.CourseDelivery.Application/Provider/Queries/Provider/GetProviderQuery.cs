d78095eada752c16a64ad54f2eba70f0a7d04df6
Scott Wakefield <scott@expertcc.co.uk>
2020-09-03 09:23:30 +0100

    FAT2-171 added query handler.

---
+++ src/SFA.DAS.CourseDelivery.Application/Provider/Queries/Provider/GetProviderQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SFA.DAS.CourseDelivery.Application.Provider.Queries.Provider
+{
+    public class GetProviderQuery: IRequest<GetProviderResponse>
+    {
+        public int Ukprn { get; set; }
+    }
+}
