65163700dc3a16e1397164a0b364b7035d1289c9
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-09-21 08:16:54 +0100

    Add new entities for provider feedback

---
+++ src/SFA.DAS.CourseDelivery.Domain/Entities/ProviderRegistrationFeedbackAttributeImport.cs
@@ -0,0 +1,7 @@
+namespace SFA.DAS.CourseDelivery.Domain.Entities
+{
+    public class ProviderRegistrationFeedbackAttributeImport : ProviderRegistrationFeedbackAttributeBase
+    {
+        
+    }
+}
---
+++ src/SFA.DAS.CourseDelivery.Domain/Entities/ProviderRegistrationFeedbackRatingBase.cs
@@ -0,0 +1,9 @@
+namespace SFA.DAS.CourseDelivery.Domain.Entities
+{
+    public class ProviderRegistrationFeedbackRatingBase
+    {
+        public int Ukprn { get; set; }
+        public string FeedbackName { get; set; }
+        public int FeedbackCount { get; set; }
+    }
+}
