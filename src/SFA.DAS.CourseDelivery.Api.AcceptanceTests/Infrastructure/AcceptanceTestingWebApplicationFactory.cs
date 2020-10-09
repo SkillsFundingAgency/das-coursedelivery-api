ddd23604936c7acb4573656212b95deaf7c1e74f
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-10-09 15:41:28 +0100

    Set up AcceptanceTest project and create Specflow structure

---
+++ src/SFA.DAS.CourseDelivery.Api.AcceptanceTests/Infrastructure/AcceptanceTestingWebApplicationFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.CourseDelivery.Api.AcceptanceTests.Infrastructure
+{
+    class AcceptanceTestingWebApplicationFactory
+    {
+    }
+}
---
+++ src/SFA.DAS.CourseDelivery.Api.AcceptanceTests/Steps/CourseProvidersSteps.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SFA.DAS.CourseDelivery.Api.AcceptanceTests
+{
+    public class CourseProvidersSteps
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
