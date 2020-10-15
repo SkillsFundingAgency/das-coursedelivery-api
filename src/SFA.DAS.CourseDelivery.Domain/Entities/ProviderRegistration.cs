f6f6c97986a118c9748f3bbfab6acb5f25b207c3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-10-15 19:44:21 +0100

    Add mapping from import to main type for provider registration

--- src/SFA.DAS.CourseDelivery.Domain/Entities/ProviderRegistration.cs
+++ src/SFA.DAS.CourseDelivery.Domain/Entities/ProviderRegistration.cs
@@ -7,5 +7,18 @@ namespace SFA.DAS.CourseDelivery.Domain.Entities
         public virtual Provider Provider { get ; set ; }
         public virtual IEnumerable<ProviderRegistrationFeedbackAttribute> ProviderRegistrationFeedbackAttributes { get ; set ; }
         public virtual IEnumerable<ProviderRegistrationFeedbackRating> ProviderRegistrationFeedbackRating { get ; set ; }
+        
+        public static implicit operator ProviderRegistration(ProviderRegistrationImport source)
+        {
+            return new ProviderRegistration
+            {
+                Ukprn = source.Ukprn,
+                FeedbackTotal = source.FeedbackTotal,
+                StatusDate = source.StatusDate,
+                StatusId = source.StatusId,
+                OrganisationTypeId = source.OrganisationTypeId,
+                ProviderTypeId = source.ProviderTypeId
+            };
+        }
     }
 }
