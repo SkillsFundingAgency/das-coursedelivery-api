e66487634b6ae0209ebe21ea4101e96c825f85f4
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-11 07:18:38 +0000

    Move BuildProviderLocation into its own extension

--- src/SFA.DAS.CourseDelivery.Application/Provider/Services/ProviderService.cs
+++ src/SFA.DAS.CourseDelivery.Application/Provider/Services/ProviderService.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SFA.DAS.CourseDelivery.Domain.Entities;
+using SFA.DAS.CourseDelivery.Domain.Extensions;
 using SFA.DAS.CourseDelivery.Domain.Interfaces;
 using SFA.DAS.CourseDelivery.Domain.Models;
 
@@ -22,20 +23,11 @@ namespace SFA.DAS.CourseDelivery.Application.Provider.Services
             _nationalAchievementRateOverallRepository = nationalAchievementRateOverallRepository;
         }
 
-        public async Task<IEnumerable<ProviderSummary>> GetRegisteredProviders()
-        {
-            var providersFromRepo = await _providerRepository.GetAllRegistered();
-
-            return providersFromRepo.Select(provider => (ProviderSummary)provider);
-        }
-
         public async Task<IEnumerable<ProviderLocation>> GetProvidersByStandardId(int standardId, string sectorSubjectArea, short level)
         {
             var providers = (await _providerRepository.GetByStandardId(standardId, sectorSubjectArea, level)).ToList();
-
-            var providerLocations = BuildProviderLocations(providers);
             
-            return providerLocations;
+            return providers.BuildProviderLocations();
         }
 
         public async Task<ProviderLocation> GetProviderByUkprnAndStandard(int ukPrn, int standardId, double? lat, double? lon, string sectorSubjectArea)
@@ -44,13 +36,20 @@ namespace SFA.DAS.CourseDelivery.Application.Provider.Services
             {
                 var providerResult = await _providerRepository.GetByUkprnAndStandardId(ukPrn, standardId, sectorSubjectArea);
 
-                return BuildProviderLocations(providerResult).FirstOrDefault();
+                return providerResult.BuildProviderLocations().FirstOrDefault();
             }
             
             var provider = await _providerRepository.GetProviderByStandardIdAndLocation(ukPrn, standardId, lat.Value, lon.Value, sectorSubjectArea);
             
-            return BuildProviderLocations(provider).FirstOrDefault();
+            return provider.BuildProviderLocations().FirstOrDefault();
             
+        }
+
+        public async Task<IEnumerable<ProviderSummary>> GetRegisteredProviders()
+        {
+            var providersFromRepo = await _providerRepository.GetAllRegistered();
+
+            return providersFromRepo.Select(provider => (ProviderSummary)provider);
         }
 
         public async Task<IEnumerable<NationalAchievementRateOverall>> GetOverallAchievementRates(string description)
@@ -71,46 +70,7 @@ namespace SFA.DAS.CourseDelivery.Application.Provider.Services
         {
             var providers = await _providerRepository.GetByStandardIdAndLocation(standardId, lat, lon, querySortOrder, sectorSubjectArea, level);
 
-            var providerLocations = BuildProviderLocations(providers);
-            
-            return providerLocations;
-        }
-
-        private static IEnumerable<ProviderLocation> BuildProviderLocations(IEnumerable<ProviderWithStandardAndLocation> providers)
-        {
-            return providers
-                .GroupBy(item => new
-                {
-                    UkPrn = item.Ukprn, 
-                    item.Name, 
-                    item.TradingName,
-                    item.ContactUrl, 
-                    item.Email, 
-                    item.Phone, 
-                    item.ProviderDistanceInMiles,
-                    item.ProviderHeadOfficeAddress1,
-                    item.ProviderHeadOfficeAddress2,
-                    item.ProviderHeadOfficeAddress3,
-                    item.ProviderHeadOfficeAddress4,
-                    item.ProviderHeadOfficeTown,
-                    item.ProviderHeadOfficePostcode
-                })
-                .Select(group => new ProviderLocation(
-                    group.Key.UkPrn, 
-                    group.Key.Name,
-                    group.Key.TradingName,
-                    group.Key.ContactUrl, 
-                    group.Key.Phone, 
-                    group.Key.Email, 
-                    group.Key.ProviderDistanceInMiles,
-                    group.Key.ProviderHeadOfficeAddress1,
-                    group.Key.ProviderHeadOfficeAddress2,
-                    group.Key.ProviderHeadOfficeAddress3,
-                    group.Key.ProviderHeadOfficeAddress4,
-                    group.Key.ProviderHeadOfficeTown,
-                    group.Key.ProviderHeadOfficePostcode,
-                    group.ToList()))
-                .ToList();
+            return providers.BuildProviderLocations();
         }
 
         public async Task<Domain.Entities.Provider> GetProviderByUkprn(int ukprn)
@@ -133,5 +93,6 @@ namespace SFA.DAS.CourseDelivery.Application.Provider.Services
                 UkprnsFilteredByStandard = ukrpnsForStandard.Result
             };
         }
+
     }
 }
