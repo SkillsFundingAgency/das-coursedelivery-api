931f742a273794e26f7b5928b9955cad49b8855f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-19 09:46:50 +0000

    Correct sql join for distances

--- src/SFA.DAS.CourseDelivery.Data/Repository/ShortlistRepository.cs
+++ src/SFA.DAS.CourseDelivery.Data/Repository/ShortlistRepository.cs
@@ -139,7 +139,7 @@ from Provider P
                  from [StandardLocation] l
                 inner join ProviderStandardLocation pslx on pslx.LocationId = l.LocationId
         inner join Shortlist slx on slx.StandardId = pslx.StandardId and slx.UkPrn = pslx.UkPrn
-        and slx.ShortlistUserId = {userId}
+        and slx.Long is not null and slx.lat is not null and slx.ShortlistUserId = {userId}
         ) l on l.LocationId = psl.LocationId
 where
   PR.StatusId = 1 AND PR.ProviderTypeId = 1 and Shl.ShortlistUserId = {userId}";
