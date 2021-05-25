32894fbbad212153b92d7bc6475d3941caf4978a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-25 14:49:52 +0100

    FAT2-457 - Add logic for filtering delivery modes

--- src/SFA.DAS.CourseDelivery.Api.UnitTests/ApiResponses/WhenMappingShortlistModelToShortlistApiResponse.cs
+++ src/SFA.DAS.CourseDelivery.Api.UnitTests/ApiResponses/WhenMappingShortlistModelToShortlistApiResponse.cs
@@ -1,4 +1,5 @@
-﻿using AutoFixture.NUnit3;
+﻿using System.Collections.Generic;
+using AutoFixture.NUnit3;
 using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.CourseDelivery.Api.ApiResponses;
@@ -19,6 +20,90 @@ namespace SFA.DAS.CourseDelivery.Api.UnitTests.ApiResponses
             actual.CourseId.Should().Be(source.StandardId);
             actual.LocationDescription.Should().Be(source.LocationDescription);
             actual.CreatedDate.Should().Be(source.CreatedDate);
+        }
+
+        [Test, AutoData]
+        public void Then_The_DeliveryTypes_Are_Filtered_If_The_Distance_Exceeds_The_Radius_And_There_Is_A_Location(
+            Shortlist source,
+            DeliveryType deliveryType1,
+            DeliveryType deliveryType2,
+            DeliveryType deliveryType3)
+        {
+            //Arrange
+            deliveryType1.Radius = 5;
+            deliveryType1.DistanceInMiles = 10;
+            source.ProviderLocation.DeliveryTypes = new List<DeliveryType>
+            {
+                deliveryType1,
+                deliveryType2,
+                deliveryType3
+            };
+            
+            //Act
+            var actual = (GetShortlistItemResponse) source;
+            
+            //Assert
+            actual.ProviderDetails.DeliveryTypes.Should().BeEquivalentTo(new List<DeliveryType>
+            {
+                deliveryType2,
+                deliveryType3
+            });
+        }
+
+        [Test, AutoData]
+        public void Then_If_There_Are_No_DeliveryModes_After_Filtering_And_There_Is_A_Location_Then_Not_Found_Delivery_Mode_Added(
+            Shortlist source,
+            DeliveryType deliveryType1)
+        {
+            //Arrange
+            deliveryType1.Radius = 5;
+            deliveryType1.DistanceInMiles = 10;
+            source.ProviderLocation.DeliveryTypes = new List<DeliveryType>
+            {
+                deliveryType1
+            };
+            
+            //Act
+            var actual = (GetShortlistItemResponse) source;
+            
+            //Assert
+            actual.ProviderDetails.DeliveryTypes.Should().BeEquivalentTo(new List<DeliveryType>
+            {
+                new DeliveryType
+                {
+                    DeliveryModes = "NotFound"
+                }
+            });
+        }
+
+        [Test, AutoData]
+        public void Then_If_No_Location_Then_Delivery_Modes_Are_Not_Filtered(
+            Shortlist source,
+            DeliveryType deliveryType1,
+            DeliveryType deliveryType2,
+            DeliveryType deliveryType3)
+        {
+            //Arrange
+            source.LocationDescription = string.Empty;
+            deliveryType1.Radius = 5;
+            deliveryType1.DistanceInMiles = 10;
+            source.ProviderLocation.DeliveryTypes = new List<DeliveryType>
+            {
+                deliveryType1,
+                deliveryType2,
+                deliveryType3
+            };
+            
+            //Act
+            var actual = (GetShortlistItemResponse) source;
+            
+            //Assert
+            actual.ProviderDetails.DeliveryTypes.Should().BeEquivalentTo(new List<DeliveryType>
+            {
+                deliveryType1,
+                deliveryType2,
+                deliveryType3
+            });
         }
     }
 }
