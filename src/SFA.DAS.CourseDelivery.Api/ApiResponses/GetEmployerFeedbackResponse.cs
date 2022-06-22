ab41f1966e39a1b847f645476e8804bbf3478d1e
codewolf86 <chris@codewolfconsulting.com>
2022-06-22 15:44:01 +0100

    Update to instantiation

--- src/SFA.DAS.CourseDelivery.Api/ApiResponses/GetEmployerFeedbackResponse.cs
+++ src/SFA.DAS.CourseDelivery.Api/ApiResponses/GetEmployerFeedbackResponse.cs
@@ -6,17 +6,18 @@ namespace SFA.DAS.CourseDelivery.Api.ApiResponses
 {
     public class GetEmployerFeedbackResponse
     {
-        private ProviderLocation provider;
-
-        public GetEmployerFeedbackResponse(ProviderLocation provider)
-        {
-            this.provider = provider;
-            FeedbackAttributes = provider.FeedbackAttributes.Select(x => (GetFeedbackAttributeResponse)x).ToList();
-            FeedbackRatings = provider.FeedbackRating.Select(x => (GetFeedbackRatingResponse)x).ToList();
-        }
-
         public List<GetFeedbackAttributeResponse> FeedbackAttributes { get; set; }
         public List<GetFeedbackRatingResponse> FeedbackRatings { get; set; }
 
+        public GetEmployerFeedbackResponse(ProviderLocation provider)
+        {
+            FeedbackAttributes = provider?.FeedbackAttributes != null
+                ? provider.FeedbackAttributes.Select(x => (GetFeedbackAttributeResponse)x).ToList()
+                : new List<GetFeedbackAttributeResponse>();
+
+            FeedbackRatings = provider?.FeedbackRating != null
+                ? provider.FeedbackRating.Select(x => (GetFeedbackRatingResponse)x).ToList()
+                : new List<GetFeedbackRatingResponse>();
+        }
     }
 }
